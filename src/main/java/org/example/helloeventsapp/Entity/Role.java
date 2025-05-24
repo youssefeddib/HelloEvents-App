3226edc3c3ef3f2f8c08b94d243a5534a2291480
youssef eddib <eddiby79@gmail.com>
2025-05-24 14:16:56 +0200

    creation d'entity pour role

--- src/main/java/org/example/helloeventsapp/Entity/Role.java
+++ src/main/java/org/example/helloeventsapp/Entity/Role.java
@@ -1,1 +1,37 @@
+package org.example.helloeventsapp.Entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
