af5d8ce98f30e807a5bd2bdcd520bdec357cb313
youssef eddib <eddiby79@gmail.com>
2025-05-24 14:21:24 +0200

    creation du service de role

---
+++ src/main/java/org/example/helloeventsapp/Service/RoleService.java
@@ -0,0 +1,26 @@
+package org.example.helloeventsapp.Service;
+
+import org.example.helloeventsapp.Entity.Role;
+import org.example.helloeventsapp.Repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public void createDefaultRoles() {
+        if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+            Role userRole = new Role();
+            userRole.setName("ROLE_USER");
+            roleRepository.save(userRole);
+        }
+        if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+            Role adminRole = new Role();
+            adminRole.setName("ROLE_ADMIN");
+            roleRepository.save(adminRole);
+        }
+    }
+}
