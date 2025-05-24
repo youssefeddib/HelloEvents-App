b893e76aec791d82180e83367ca151e24f8f026a
youssef eddib <eddiby79@gmail.com>
2025-05-25 00:46:29 +0200

    creation Authcontroller pour register

--- src/main/java/org/example/helloeventsapp/Controller/AuthController.java
+++ src/main/java/org/example/helloeventsapp/Controller/AuthController.java
@@ -1,1 +1,35 @@
+package org.example.helloeventsapp.Controller;
+
+import org.example.helloeventsapp.Dto.RegisterRequest;
+import org.example.helloeventsapp.Dto.LoginRequest;
+import org.example.helloeventsapp.Service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest request) {
+        try {
+            String jwt = authService.register(request);
+            return ResponseEntity.ok("Inscription réussie. Token: " + jwt);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Erreur d'inscription: " + e.getMessage());
+        }
+    }
+
+
+}
 
