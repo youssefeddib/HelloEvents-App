b21526f79a443a9c3e17e7cbe1cf3f794db09875
youssef eddib <eddiby79@gmail.com>
2025-05-25 14:56:11 +0200

    jwtutil security

--- src/main/java/org/example/helloeventsapp/Security/JWTUtil.java
+++ src/main/java/org/example/helloeventsapp/Security/JWTUtil.java
@@ -1,1 +1,42 @@
+package org.example.helloeventsapp.Security;
+
+import io.jsonwebtoken.*;
+import org.example.helloeventsapp.Service.JWTService;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    private static final String SECRET_KEY = "mySecretKey12345";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 10;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (JwtException e) {
+            throw new RuntimeException("Invalid JWT Token");
+        }
+    }
+}
 
